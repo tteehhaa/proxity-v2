f423a8e893aa6706ecc8521519e17bd111318f61
tteehhaa <hanabeom@gmail.com>
2025-05-21 23:28:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ if submitted:
     df.loc[mask_est, '실사용가격'] = df['2025.05_보정_추정실거래가']
     df.loc[mask_est, '가격출처'] = '추정'
 
-# 추정가도 포함한 예산 상한 필터링
+    # 추정가도 포함한 예산 상한 필터링
     df = df[df['2025.05_보정_추정실거래가'] <= budget_cap]
 
 
@@ -145,11 +145,11 @@ if submitted:
     # 너무 비싼 추정 단지 제외
     df = df[~((df['가격출처'] == '추정') & (df['실사용가격'] > budget_upper))]
 
+     # top3 항상 초기화 (조건문 진입 안 해도 이후에서 참조 가능하게 하기 위함)
+    top3 = pd.DataFrame()
+
     # 예산 범위 내 단지 필터링
     df_filtered = df[df['실사용가격'] <= budget_upper].copy()
-
-    # top3 항상 초기화 (조건문 진입 안 해도 이후에서 참조 가능하게 하기 위함)
-    top3 = pd.DataFrame()
 
     # 3개 미만일 경우 예산 초과 일부 포함
     if len(df_filtered) < 3:
